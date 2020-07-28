8a01a4c6b935492840bc1270b62b0a01277cc997
Milton Torasso <miltontorasso@hotmail.com>
2020-07-28 19:43:27 -0300

    Release tentativo
    Implementada la funcionalidad para que pueda operar tanto un sensor de luz si se lo configura como sensor como un relay con una lámpara si se lo configura como actuador.

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,46 @@
 #include <ArduinoJson.h>
 #include <FS.h>
 #include <ESP8266TrueRandom.h>
+#include <Wire.h>
+#include <BH1750.h>
 
-char* server = "192.168.22.6";
+//Global Variables
+char* server = "35.208.43.150"; //Server IP
+int pinDispositivo = 2; //Pin Relay
+const char* dataTopic = "";
+const char* tipoDispositivo = "";
+const char* nombreDispositivo = "";
 
-StaticJsonDocument<200> jsonDocument;
-JsonArray array;
+BH1750 lightMeter;
+StaticJsonDocument<200> configJson;
 WiFiClient wifiClient;
 WiFiUDP Udp;
 PubSubClient client(wifiClient);
 
-void setup() {
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensaje:");
+  String mensaje = "";
+  for (int i = 0; i < length; i++) {
+    mensaje += (char)payload[i];
+  }
+  mensaje.trim();
+  Serial.println(mensaje);
+  if (mensaje.equals("1")){
+    Serial.println("Encendiendo Actuador!");
+    digitalWrite(pinDispositivo, LOW);
+  }
+  else if  (mensaje.equals("0")){
+    Serial.println("Apagando Actuador!");
+    digitalWrite(pinDispositivo, HIGH);
+  }
+}
 
+void setup() {
   Serial.begin(115200);
 
-// WiFi Settings
-  const char* ssid     = "LSI-4"; // WIFI SSID
-  const char* password = "lsi18lsi"; // WIFI PASSWORD
+  // WiFi Settings
+  const char* ssid     = "SSID"; // WIFI SSID
+  const char* password = "PASSWORD"; // WIFI PASSWORD
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.print("Intentando conectarse a la red: ");
@@ -35,10 +59,9 @@ void setup() {
   Serial.print(WiFi.localIP());
   Serial.println("");
 
-
-// uuid 
+  // uuid File
   SPIFFS.begin();
-  SPIFFS.format();
+  //SPIFFS.format();
   String uuidStr = "";
   if (!SPIFFS.exists("/uuid.txt")) {
     Serial.println("Archivo /uuid.txt no encontrado, generando uno nuevo.");
@@ -65,19 +88,22 @@ void setup() {
   Serial.print(uuidStr);
   Serial.println("");
 
-// config
+  // Config File
   String configStr = "";
   if (!SPIFFS.exists("/config.txt")) {
     Serial.println("Archivo de configuración no encontrado, solicitando configuración");
     const unsigned int udpServerPort = 6789;
-    const unsigned int udpLocalPort = 2500;
+    const unsigned int udpLocalPort = 2500; //Debe ser único entre dispositivos detrás de NAT
+    uuidStr.trim();
+    String localPortStr = String(udpLocalPort); 
     Udp.begin(udpLocalPort);
     String bcastString = "{\"dispositivo\":{\"mac\":\"";
     bcastString += WiFi.macAddress();
     bcastString += "\",\"uuid\":\"";
-    bcastString += "9862d20e-6c84-4887-9cb8-855693f6b9c7";
-    //bcastString += uuidStr;
-    bcastString += "\"}}";
+    bcastString += uuidStr;
+    bcastString += "\",\"puerto\":";
+    bcastString += localPortStr;
+    bcastString += "}}";
     char bcastPacket[255] = "";
     bcastString.toCharArray(bcastPacket, 255);
     int configured = 0;
@@ -87,7 +113,7 @@ void setup() {
       Serial.println(bcastPacket);
       Udp.write(bcastPacket);
       Udp.endPacket();
-      Serial.println("Paquete de descubrimiento enviado!\n");
+      Serial.println("Paquete de descubrimiento enviado!");
       int packetSize = Udp.parsePacket();
       if (packetSize) {
         char incomingPacket[255];
@@ -113,9 +139,65 @@ void setup() {
   Serial.print("Configuración: ");
   Serial.print(configStr);
   Serial.println("");
+
+  //JSON Parse
+  DeserializationError err = deserializeJson(configJson, configStr);
+  if(err) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(err.c_str());
+  }
+  else {
+    nombreDispositivo = configJson["nombreDispositivo"];
+    tipoDispositivo = configJson["tipoDispositivo"];
+    dataTopic = configJson["topic"];
+    Serial.println("Json deserializado con exito\n");
+  }
+
+  // Configuración MQTT Broker
+  Serial.println("Conectandose al MQTT Broker");
+  client.setServer(server, 1883);
+  while (!client.connected()) {
+    Serial.println("Conectando al Broker MQTT...");
+    if (client.connect(nombreDispositivo)) {
+      Serial.println("Conexión Exitosa");  
+    } 
+    else {
+      Serial.print("Conexión falló con el estado ");
+      Serial.print(client.state());
+      Serial.println();
+      delay(2000);
+    }
+  }
+
+  //Configuración Dispositivo
+  if (strcmp(tipoDispositivo,"SENSOR") == 0) {
+    Serial.println("tipoDispositivo es SENSOR");
+    Wire.begin();
+    if (lightMeter.begin()) {
+      Serial.println("Sensor de Luz inicializado");
+    }
+    else {
+      Serial.println("Error inicializando Sensor de Luz");
+    }
+  }
+  else if (strcmp(tipoDispositivo,"ACTUADOR") == 0) {
+    Serial.println("tipoDispositivo es ACTUADOR");
+    client.setCallback(callback);
+    pinMode(pinDispositivo, OUTPUT);
+  }
+  else {
+    Serial.println("tipoDispositivo no reconocido");
+  }
 }
 
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (strcmp(tipoDispositivo,"SENSOR") == 0) {
+    char* luxStr = "";
+    uint16_t lux = lightMeter.readLightLevel();
+    sprintf(luxStr, "%u", lux);
+    Serial.printf("Light: %s lx\n",luxStr);
+    client.publish(dataTopic,luxStr);
+    delay(1000);
+  }
+  client.loop();
 }
